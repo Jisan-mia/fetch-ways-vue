33ade7cfcfe89f8e7fd46c20353e446658a8d3b1
Jisan-mia <jisanmia47@gmail.com>
2021-12-10 00:41:23 +0600

    example with axios

--- src/components/AxiosExample.vue
+++ src/components/AxiosExample.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="container">
+  <div v-for="todo in todos" :key="todo.id" class="container">
     <div class="item">
-      item
+      {{ todo.title }}
     </div>
   </div>
 </template>
 
 <script>
+import getAxiosData from '../composables/getAxiosData'
 export default {
   name: 'AxiosExample',
   setup() {
     
+    const {todos, errors, loadTodos} = getAxiosData()
+    loadTodos()
 
-    return {}
+
+    return { todos , errors}
   }
 }
 </script>
--- src/composables/getAxiosData.js
+++ src/composables/getAxiosData.js
@@ -2,5 +2,21 @@ import { ref } from 'vue'
 import axios from 'axios';
 
 const fetAxiosData = () => {
-  const totods = ref([])
+  const todos = ref([]);
+  const error = ref(null);
+
+  const loadTodos = async () => {
+    try{
+      const data = await axios.get('https://jsonplaceholder.typicode.com/todos');
+      if(!data.ok) {
+        throw Error('Error getting Data');
+      }
+      todos.value = await data.json();
+    } catch (err) {
+      error.value = err.message;
+      console.log(err.message)
+    }
+  }
+  return {todos, error, loadTodos}
 }
+export default getFetchData;
